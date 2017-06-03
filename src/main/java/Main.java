250853e3bdcbbf7cbf83b11f1ef5d6e8e8688fad
tan4ek <ssr205@yandex.ru>
2017-06-03 20:00:41 +0300

    Убранно лишнее, пример properties файла

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,17 @@
 import com.yandex.disk.rest.Credentials;
-import com.yandex.disk.rest.ProgressListener;
 import com.yandex.disk.rest.ResourcesArgs;
 import com.yandex.disk.rest.RestClient;
 import com.yandex.disk.rest.exceptions.ServerException;
 import com.yandex.disk.rest.exceptions.http.HttpCodeException;
-import com.yandex.disk.rest.json.DiskInfo;
 import com.yandex.disk.rest.json.Link;
 import com.yandex.disk.rest.json.Resource;
+import org.apache.commons.lang3.BooleanUtils;
 import org.apache.commons.lang3.StringUtils;
 
-import java.io.*;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -17,7 +19,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 import java.util.zip.Adler32;
 import java.util.zip.CheckedOutputStream;
 import java.util.zip.ZipEntry;
@@ -29,30 +34,16 @@ import java.util.zip.ZipOutputStream;
 public class Main {
 
     public static void main(String[] args) throws IOException, ServerException {
-        File f = new File(".");
-        String files[] = f.list();
-        System.out.println(Arrays.toString(files));
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy--HH-mm");
-        System.out.println(dateTimeFormatter.format(LocalDateTime.now()));
-//        if (true)
-//            return;
-
         Properties properties = readProperties(args);
-        if (properties == null) return;
 
         Credentials credentials = new Credentials(properties.getProperty("user"), properties.getProperty("otoken"));
 
-//        OkHttpClient okHttpClient = OkHttpClientFactory.makeClient()
-
         RestClient client = new RestClient(credentials);
-
-        DiskInfo diskInfo = client.getDiskInfo();
 
 
         String backupFolderPath = properties.getProperty("yadisk.backup.folder");
         try {
             Resource bacupFolder = client.getResources(new ResourcesArgs.Builder().setPath(backupFolderPath).build());
-//            client
         } catch (HttpCodeException e) {
             if (e.getCode() == HttpURLConnection.HTTP_NOT_FOUND) {
                 //creating folder
@@ -95,54 +86,15 @@ public class Main {
             achived.put(archivePath, zipName);
         }
 
+        boolean removeAfterBackup = BooleanUtils.toBoolean(properties.getProperty("remove.after.backup", "false"));
+
         for (Path archive : achived.keySet()) {
-
             Link uploadLink = client.getUploadLink(backupFolderPath + "/" + achived.get(archive), false);
-            client.uploadFile(uploadLink, false, archive.toFile(), new ProgressListener() {
-                @Override
-                public void updateProgress(long loaded, long total) {
-                    if (total != 0) {
-                        System.out.println("was load: " + (float) loaded / total);
-                    }
-                }
-
-                @Override
-                public boolean hasCancelled() {
-                    return false;
-                }
-            });
+            client.uploadFile(uploadLink, false, archive.toFile(), null);
+            if (removeAfterBackup) {
+                Files.delete(archive);
+            }
         }
-
-//        bacupFolder
-//        bacupFolder.
-//        System.out.println(bacupFolder);
-
-//        List<String> files = new ArrayList<>();
-//        FileSystems.newFileSystem(Paths.get("/media/ssr/DATA/Photos.zip"), null)
-//                .getRootDirectories().forEach(v -> {
-//            try {
-//                Files.walk(v, 1).forEach(f -> {
-//                    if (f == null || f.getFileName() == null) {
-//                        System.out.println("F is null: " + f + " ");
-//                    } else {
-//                        files.add(f.getFileName().toString());
-//                    }
-//                });
-//            } catch (IOException e) {
-//                throw new RuntimeException(e);
-//            }
-//        });
-//        System.out.println(files.get(0));
-//        System.out.println(files.size());
-//        if (true)
-//            return;
-//
-//        ResourcesArgs fotos = new ResourcesArgs.Builder().setPath("Фотокамера").setSort(ResourcesArgs.Sort.created).build();
-//        Resource resources = client.getResources(fotos);
-//        System.out.println(resources);
-//
-//        System.out.println(resources.getResourceList().getItems().size());
-//        System.out.println(diskInfo);
     }
 
     public static void archive(Path folderToArchive, Path outputPath) {
@@ -158,7 +110,6 @@ public class Main {
             Files.walk(folderToArchive).forEach(p -> {
                 if (!Files.isDirectory(p)) {
                     ZipEntry entry = new ZipEntry(folderToArchive.toAbsolutePath().relativize(p).toString());
-                    System.out.println(folderToArchive.toAbsolutePath().relativize(p).toString());
                     try {
                         out.putNextEntry(entry);
                         out.write(Files.readAllBytes(p));
@@ -171,7 +122,6 @@ public class Main {
             System.out.println(outputPath.toAbsolutePath().toString());
 
             out.close();
-            System.out.println("checksum: " + checksum.getChecksum().getValue());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -192,17 +142,16 @@ public class Main {
                 properties.load(bufferedReader);
             }
         } else {
-            System.out.println("Properties not found: " + propertiesPath);
-            return null;
+            throw new IllegalArgumentException("Properties not found: " + propertiesPath);
         }
-
         if (!properties.containsKey("otoken")) {
-            System.out.println("otoken not defined");
-            return null;
+            throw new IllegalArgumentException("otoken not defined");
         }
         if (!properties.containsKey("user")) {
-            System.out.println("user not defined");
-            return null;
+            throw new IllegalArgumentException("user not defined");
+        }
+        if (!properties.containsKey("yadisk.backup.folder")) {
+            throw new IllegalArgumentException("yadisk.backup.folder not defined");
         }
         return properties;
     }
