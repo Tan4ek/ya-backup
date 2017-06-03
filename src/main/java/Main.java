2cafd2dc44bf9aacc910f3dfadc396ade0b8213c
tan4ek <ssr205@yandex.ru>
2017-06-03 19:14:14 +0300

    Добавил загрузку по папкам (из проперти)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,41 @@
 import com.yandex.disk.rest.Credentials;
+import com.yandex.disk.rest.ProgressListener;
 import com.yandex.disk.rest.ResourcesArgs;
 import com.yandex.disk.rest.RestClient;
-import com.yandex.disk.rest.exceptions.ServerIOException;
+import com.yandex.disk.rest.exceptions.ServerException;
 import com.yandex.disk.rest.exceptions.http.HttpCodeException;
 import com.yandex.disk.rest.json.DiskInfo;
 import com.yandex.disk.rest.json.Link;
 import com.yandex.disk.rest.json.Resource;
+import org.apache.commons.lang3.StringUtils;
 
-import java.io.BufferedReader;
-import java.io.IOException;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Properties;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.zip.Adler32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by ssr on 02.06.17.
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException, ServerIOException {
+    public static void main(String[] args) throws IOException, ServerException {
+        File f = new File(".");
+        String files[] = f.list();
+        System.out.println(Arrays.toString(files));
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy--HH-mm");
+        System.out.println(dateTimeFormatter.format(LocalDateTime.now()));
+//        if (true)
+//            return;
 
         Properties properties = readProperties(args);
         if (properties == null) return;
@@ -39,13 +53,66 @@ public class Main {
         try {
             Resource bacupFolder = client.getResources(new ResourcesArgs.Builder().setPath(backupFolderPath).build());
 //            client
-        } catch (HttpCodeException e){
+        } catch (HttpCodeException e) {
             if (e.getCode() == HttpURLConnection.HTTP_NOT_FOUND) {
                 //creating folder
                 Link link = client.makeFolder(backupFolderPath);
 
             }
         }
+
+        Map<String, String> propToFolderName = new HashMap<>();
+
+        Enumeration<String> enumeration = (Enumeration<String>) properties.propertyNames();
+        while (enumeration.hasMoreElements()) {
+            String propertyName = enumeration.nextElement();
+            if (!StringUtils.startsWith(propertyName, "backup")) {
+                continue;
+            }
+            String folderName = StringUtils.substringAfter(propertyName, ".");
+            if (StringUtils.isBlank(folderName) || StringUtils.contains(folderName, ".")) {
+                throw new IllegalArgumentException("Property name " + propertyName + " not valid.");
+            }
+            propToFolderName.put(propertyName, folderName);
+        }
+
+        Map<Path, String> achived = new HashMap<>();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy--HH-mm");
+
+        for (String propName : propToFolderName.keySet()) {
+            String path = properties.getProperty(propName);
+            Path toArchive = Paths.get(path);
+            Path archivePath;
+            String zipName = propToFolderName.get(propName) + "-" + formatter.format(LocalDateTime.now()) + ".zip";
+            if (Files.isDirectory(toArchive)) {
+                archivePath = Paths.get(toArchive.toString(), zipName);
+            } else {
+                Path parent = toArchive.getParent();
+                archivePath = Paths.get(parent.toString(), zipName);
+            }
+            archive(toArchive, archivePath);
+            achived.put(archivePath, zipName);
+        }
+
+        for (Path archive : achived.keySet()) {
+
+            Link uploadLink = client.getUploadLink(backupFolderPath + "/" + achived.get(archive), false);
+            client.uploadFile(uploadLink, false, archive.toFile(), new ProgressListener() {
+                @Override
+                public void updateProgress(long loaded, long total) {
+                    if (total != 0) {
+                        System.out.println("was load: " + (float) loaded / total);
+                    }
+                }
+
+                @Override
+                public boolean hasCancelled() {
+                    return false;
+                }
+            });
+        }
+
 //        bacupFolder
 //        bacupFolder.
 //        System.out.println(bacupFolder);
@@ -76,6 +143,38 @@ public class Main {
 //
 //        System.out.println(resources.getResourceList().getItems().size());
 //        System.out.println(diskInfo);
+    }
+
+    public static void archive(Path folderToArchive, Path outputPath) {
+        try {
+            FileOutputStream dest = new
+                    FileOutputStream(outputPath.toFile());
+            CheckedOutputStream checksum = new
+                    CheckedOutputStream(dest, new Adler32());
+
+            ZipOutputStream out = new
+                    ZipOutputStream(new
+                    BufferedOutputStream(checksum));
+            Files.walk(folderToArchive).forEach(p -> {
+                if (!Files.isDirectory(p)) {
+                    ZipEntry entry = new ZipEntry(folderToArchive.toAbsolutePath().relativize(p).toString());
+                    System.out.println(folderToArchive.toAbsolutePath().relativize(p).toString());
+                    try {
+                        out.putNextEntry(entry);
+                        out.write(Files.readAllBytes(p));
+                    } catch (IOException e) {
+                        throw new RuntimeException("Reading " + p + " fail", e);
+                    }
+                }
+            });
+
+            System.out.println(outputPath.toAbsolutePath().toString());
+
+            out.close();
+            System.out.println("checksum: " + checksum.getChecksum().getValue());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
 
